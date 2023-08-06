9a3644b4aeba3366c9bc7f1c4ea80c3d1d9fa8d3
Jared Henley <jared.henley@gmail.com>
2023-08-06 20:10:11 +1000

    Updated site 2023-08- 6

--- jquiz/jquiz.js
+++ jquiz/jquiz.js
@@ -31,11 +31,280 @@ console.log(link);
 // It gets "runtime-coded" into the HTML elements so that they can always
 // look up their own quiz_id when events happen
 quiz_ids = {};
+quiz_results = {};
 // A dictionary to hold the quiz data for each URL
 quizzes = {};
 
 // Sounds to play
-audio_applause = new Audio('/jquiz/media/applause_normal.mp3');
+audio_correct = new Audio('/jquiz/media/applause_normal.mp3');
+audio_incorrect = new Audio('/jquiz/media/336998__timkahn__awww-01.mp3');
+audio_music = new Audio('/jquiz/media/sport-news-intro-01-by-taigasoundprod-from-filmmusic-io.mp3');
+
+function stop_all_audio() {
+    audio_correct.pause();
+    audio_correct.currentTime = 0;
+    audio_incorrect.pause();
+    audio_incorrect.currentTime = 0;
+    audio_music.pause();
+    audio_music.currentTime = 0;
+}
+
+function jquiz_insert(url, title, max_questions) {
+/* This function inserts a quiz from the given url onto the page -
+ * if it's not already loaded. It also creates a unique quiz_id to
+ * track the quiz on the page.
+ *
+ * It then creates a "start" button to start the quiz.
+ *
+ * title: the title of the quiz
+ * max_questions - optional: the number of questions to present.
+ * * If it's less than the number of questions in the quiz file,
+ *   randomly choose this number of questions to present.
+ * * If it's zero, present all questions.
+ */
+    // TODO: Implement max_questions random feature
+    // Get the file
+    console.log("JQuiz: Loading " + url);
+    if (! (url in quizzes)) {
+        // Only load the quiz if it's not already loaded
+        quizzes[url] = {'loaded': false}; // A placeholder to prevent dual loading while the quiz is fetched
+        rq = new XMLHttpRequest();
+        rq.onload = function() {
+            console.log("JQuiz: Parsing file for quiz at " + url);
+            quizzes[url]['quiz'] = jquiz_parse_file(this.responseText);
+            quizzes[url]['loaded'] = true;
+            console.log("JQuiz: quiz at " + url + " parsed.");
+        }
+        url_no_cache = url + "?v=" + Date.now();
+        rq.open('GET', url_no_cache, true);
+        rq.send();
+    } else {
+        console.log("JQuiz: Quiz was already loaded (or loading)");
+    }
+    // Create a unique quiz_id
+    quiz_id = jquiz_unique_id(url);
+    quiz_ids[quiz_id].title = title;
+    if (max_questions === undefined) {
+        quiz_ids[quiz_id].max_questions = 0;
+    } else {
+        quiz_ids[quiz_id].max_questions = max_questions;
+    }
+    // Create a container for the unique quiz
+    console.log("JQuiz: Creating a div container for the quiz", quiz_id);
+    document.write(`
+    <div class="jquiz-container" id="jquiz-${quiz_id}">
+    </div>`);
+    // Display a start button
+    jquiz_display_start(quiz_id);
+}
+
+function jquiz_unique_id(url) {
+    have_unique_id = false;
+    while (! have_unique_id) {
+        id = (Math.random() * 10**20).toString();
+        ids = Object.keys(quiz_ids);
+        if (! (id in ids))
+            have_unique_id = true;
+    }
+    quiz_ids[id] = {}
+    quiz_ids[id].url = url;
+    return id;
+}
+
+function write_html_to_quiz(quiz_id, html) {
+    div = document.getElementById(`jquiz-${quiz_id}`);
+    div.innerHTML = html;
+}
+
+function jquiz_display_start(quiz_id) {
+    console.log("JQuiz: Creating a start button for quiz", quiz_id);
+    title = quiz_ids[quiz_id].title;
+    html = `<p>${title}</p>
+<input type="button" value="Start the quiz" onclick="jquiz_start(${quiz_id});" />`;
+    write_html_to_quiz(quiz_id, html);
+}
+
+function jquiz_start(quiz_id) {
+    stop_all_audio();
+    audio_music.play();
+    quiz_id = quiz_id.toString();
+    console.log("JQuiz: Starting the quiz with id", quiz_id);
+    jquiz_init_quiz(quiz_id);
+    jquiz_next_question(quiz_id);
+}
+
+function jquiz_next_question(quiz_id) {
+    quiz_id = quiz_id.toString();
+    console.log("JQuiz: Presenting next question for quiz with id", quiz_id);
+    id_in_results = jquiz_key_in_dict(quiz_id, quiz_results);
+    // Check if we're continuing an existing quiz
+    if (id_in_results) {
+        console.log("JQuiz: Quiz has been started.");
+        if (quiz_results[quiz_id]['completed']) {
+            console.log("JQuiz: Quiz was completed. Displaying the final score");
+            jquiz_show_scoreboard(quiz_id);
+            return;
+        } else {
+            console.log("JQuiz: Quiz is incomplete. Continuing.");
+        }
+    } else {
+        // Initialise a new quiz
+        jquiz_init_quiz(quiz_id);
+    }
+    // OK we're good to go.
+    q_index = quiz_results[quiz_id]['q_index'];
+    q_number = q_index + 1;
+    score = quiz_results[quiz_id]['score'];
+    num_questions = quiz_results[quiz_id]['num_questions'];
+    // TODO: Might need to check that the quiz has loaded first, and do
+    // nothing otherwise, waiting for the user to click again.
+    url = quiz_ids[quiz_id].url;
+    question = quizzes[url]['quiz']['questions'][q_index];
+    // Build up some HTML for the question
+    question_html = jquiz_text_image_html(question)
+    // Build up some HTML for the answers
+    answer_code = [];
+    question.answers.forEach((answer, index) => {
+        // Not guaranteed unique but hopefully good enough
+        answer_id = (Math.random() * 10**20).toString();
+        answer_snippet = jquiz_text_image_html(answer);
+        answer_code.push(`<input type="radio" id="${answer_id}" name="jquiz-${quiz_id}-radios" correct="${answer.correct}" index="${index}" />
+        <label for="${answer_id}">${answer_snippet}</label>`);
+        });
+    answer_html = answer_code.join("\n");
+
+    // Increment the question number and check if the quiz will be completed with this question
+    quiz_results[quiz_id]['q_index']++;
+    if (quiz_results[quiz_id]['q_index'] >=
+        quiz_results[quiz_id]['num_questions']) {
+            quiz_results[quiz_id]['completed'] = true;
+            console.log("JQuiz: This is the last question. Quiz is complete.");
+        next_button_text = "Finish quiz";
+    } else {
+        next_button_text = "Next question";
+    }
+    html = `
+        <div class="jquiz-quiz">
+        <div class="jquiz-question">
+            ${question_html}
+        </div>
+        <div class="jquiz-answers">
+            ${answer_html}
+        </div>
+        <div class="jquiz-bottombar">
+            <p class="jquiz-qnum">Question ${q_number} of ${num_questions}</p>
+            <p class="jquiz-score">Score: ${score}</p>
+            <input type="button" value="Lock it in" class="jquiz-lock" onclick="jquiz_lock_it_in(${quiz_id});" />
+            <input type="button" value="${next_button_text}" class="jquiz-next" disabled onclick="jquiz_next_question(${quiz_id});" />
+        </div>
+        </div>`;
+    // Put the HTML into the quiz div
+    write_html_to_quiz(quiz_id, html);
+
+    // Enable the lock it in button
+    selector = "#jquiz-" + quiz_id + " .jquiz-lock";
+    lock_button = document.querySelector(selector);
+    lock_button.disabled = false;
+}
+
+function jquiz_text_image_html(thing) {
+    if (thing.text === undefined) {
+        text = '';
+    } else {
+        text = thing.text.trim();
+    }
+    if (thing.image === undefined) {
+        image = '';
+    } else {
+        image = thing.image.trim();
+    }
+    html = '';
+    if (text.length > 0) {
+        html += `<p>${text}</p>`;
+    }
+    if (image.length > 0) {
+        html += `<img src=${image} />`;
+    }
+    return html;
+}
+
+function jquiz_show_scoreboard(quiz_id) {
+    // Play some up-beat music
+    stop_all_audio();
+    audio_music.play();
+    // Display the score
+    score = quiz_results[quiz_id]['score'];
+    num_questions = quiz_results[quiz_id]['num_questions'];
+    html = `
+        <div class="jquiz-quiz">
+        <div class="jquiz-scoreboard">
+            <p>You scored<br><span class="score">${score}</span><br> out of ${num_questions}</p>
+        </div>
+        <div class="jquiz-bottombar">
+            <input type="button" value="close" class="jquiz-close" onclick="jquiz_display_start(${quiz_id});" />
+        </div>
+        </div>`;
+    // Put the HTML into the quiz div
+    write_html_to_quiz(quiz_id, html);
+}
+
+function jquiz_key_in_dict(key, dict) {
+    // The following code is required because checking if key is in
+    // dict, for quizzes, always gives false, even when it should be true
+    id_in_results = false;
+    ids = Object.keys(dict);
+    ids.forEach(k => {
+        if (key == k)
+            id_in_results = true;
+    });
+    return id_in_results
+}
+
+function jquiz_init_quiz(quiz_id) {
+    console.log("JQuiz: Starting quiz");
+    url = quiz_ids[quiz_id].url;
+    quiz_results[quiz_id] = {
+        'q_index': 0,
+        'completed': false,
+        'score': 0,
+        'num_questions': quizzes[url]['quiz']['questions'].length};
+}
+
+function jquiz_lock_it_in(quiz_id) {
+    // Check the answer, update the score etc.
+    user_correct = true;
+    selector = "#jquiz-" + quiz_id + " input[type=radio]";
+    answers = document.querySelectorAll(selector);
+    answers.forEach(answer => {
+        answer.disabled = true;
+        is_answer = (answer.getAttribute("correct") == "true");
+        if (is_answer)
+            answer.classList.add("correct");
+        else
+            answer.classList.add("incorrect");
+        right_answer = (is_answer == answer.checked);
+        user_correct = user_correct && right_answer;
+    });
+    stop_all_audio();
+    if (user_correct) {
+        quiz_results[quiz_id]['score']++;
+        audio_correct.play();
+    } else {
+        audio_incorrect.play();
+    }
+    console.log("User is", user_correct);
+    // Enable the "next question" button.
+    selector = "#jquiz-" + quiz_id + " .jquiz-next";
+    next_button = document.querySelector(selector);
+    next_button.disabled = false;
+    selector = "#jquiz-" + quiz_id + " .jquiz-lock";
+    lock_button = document.querySelector(selector);
+    lock_button.disabled = true;
+    selector = "#jquiz-" + quiz_id + " .jquiz-score";
+    score_text = document.querySelector(selector);
+    score_text.innerHTML = "Score: " + quiz_results[quiz_id]['score'];
+}
+
 
 function jquiz_is_new_question(line) {
     return line.startsWith('%') && ! line.startsWith('%%');
@@ -119,197 +388,4 @@ function jquiz_parse_file(text) {
         quiz['questions'].push(question);
     });
     return quiz;
-}
-
-/*
-function jquiz_quiz_url_loaded() {
-    // Create a quiz ID
-    //quiz_id = jquiz_unique_id();
-    //quiz_ids.push(quiz_id);
-    console.log("JQuiz: Parsing file for quiz " + quiz_id);
-    quiz = jquiz_parse_file(this.responseText);
-    console.log(quiz);
-    console.log("JQuiz: quiz " + quiz_id + " parsed.");
-}
-*/
-
-function jquiz_load(url) {
-    // Get the file
-    console.log("JQuiz: Loading " + url);
-    if (! (url in quizzes)) {
-        // Only load the quiz if it's not already loaded
-        quizzes[url] = {'loaded': false}; // A placeholder to prevent dual loading while the quiz is fetched
-        rq = new XMLHttpRequest();
-        rq.onload = function() {
-            console.log("JQuiz: Parsing file for quiz at " + url);
-            quizzes[url]['quiz'] = jquiz_parse_file(this.responseText);
-            quizzes[url]['loaded'] = true;
-            console.log("JQuiz: quiz at " + url + " parsed.");
-        }
-        url_no_cache = url + "?v=" + Date.now();
-        rq.open('GET', url_no_cache, true);
-        rq.send();
-    } else {
-        console.log("JQuiz: Quiz was already loaded (or loading)");
-    }
-    // Display a start button
-    jquiz_create_html(url);
-}
-
-function jquiz_unique_id(url) {
-    have_unique_id = false;
-    while (! have_unique_id) {
-        id = (Math.random() * 10**20).toString();
-        ids = Object.keys(quiz_ids);
-        if (! (id in ids))
-            have_unique_id = true;
-    }
-    quiz_ids[id] = url;
-    return id;
-}
-
-quiz_results = {};
-
-function jquiz_create_html(url) {
-    quiz_id = jquiz_unique_id(url);
-    console.log("JQuiz: Creating HTML elements for quiz", quiz_id);
-    document.write(`
-    <div class="jquiz-container" id="jquiz-${quiz_id}">
-        <input type="button" value="Start the quiz" onclick="jquiz_next_question(${quiz_id});" />
-    </div>`);
-}
-
-function jquiz_next_question(quiz_id) {
-    quiz_id = quiz_id.toString();
-    console.log("JQuiz: Presenting next question for quiz with id", quiz_id);
-    url = quiz_ids[quiz_id];
-    id_in_results = jquiz_key_in_dict(quiz_id, quiz_results);
-    // Check if we're continuing an existing quiz
-    if (id_in_results) {
-        console.log("JQuiz: Quiz has been started.");
-        if (quiz_results[quiz_id]['completed']) {
-            console.log("JQuiz: Quiz was completed. Displaying the final score");
-            jquiz_show_scoreboard(quiz_id);
-            return;
-        } else {
-            console.log("JQuiz: Quiz is incomplete. Continuing.");
-        }
-    } else {
-        // Initialise a new quiz
-        jquiz_init_quiz(quiz_id);
-    }
-    // OK we're good to go.
-    q_index = quiz_results[quiz_id]['q_index'];
-    q_number = q_index + 1;
-    score = quiz_results[quiz_id]['score'];
-    num_questions = quiz_results[quiz_id]['num_questions'];
-    // TODO: Might need to check that the quiz has loaded first, and do
-    // nothing otherwise, waiting for the user to click again.
-    question = quizzes[url]['quiz']['questions'][q_index];
-    // Build up some HTML for the question
-    question_html = `
-            <h1>${question.text}<h1>
-            <img src=${question.image} />`;
-    // Build up some HTML for the answers
-    answer_code = [];
-    question.answers.forEach((answer, index) => {
-        // Not guaranteed unique but hopefully good enough
-        answer_id = (Math.random() * 10**20).toString();
-        answer_code.push(`
-            <input type="radio" id="${answer_id}" name="jquiz-${quiz_id}-radios" correct="${answer.correct}" index="${index}" />
-            <label for="${answer_id}">${answer.text}</label>`);
-        });
-    answer_html = answer_code.join("\n");
-    html = `
-        <div class="jquiz-quiz">
-        <div class="jquiz-question">
-            ${question_html}
-        </div>
-        <div class="jquiz-answers">
-            ${answer_html}
-        </div>
-        <div class="jquiz-bottombar">
-            <p class="jquiz-qnum">Question ${q_number} of ${num_questions}</p>
-            <p class="jquiz-score">Score: ${score}</p>
-            <input type="button" value="Lock it in" class="jquiz-lock" onclick="jquiz_lock_it_in(${quiz_id});" />
-            <input type="button" value="Next question" class="jquiz-next" disabled onclick="jquiz_next_question(${quiz_id});" />
-        </div>
-        </div>`;
-    // Put the HTML into the quiz div
-    div = document.getElementById(`jquiz-${quiz_id}`);
-    div.innerHTML = html;
-
-    // Increment the question number and check if the quiz has been completed
-    quiz_results[quiz_id]['q_index']++;
-    if (quiz_results[quiz_id]['q_index'] >=
-        quiz_results[quiz_id]['num_questions']) {
-            quiz_results[quiz_id]['completed'] = true;
-            console.log("JQuiz: That was the last question. Quiz is complete.");
-    }
-
-    // Enable the lock it in button
-    selector = "#jquiz-" + quiz_id + " .jquiz-lock";
-    lock_button = document.querySelector(selector);
-    lock_button.disabled = false;
-}
-
-function jquiz_show_scoreboard(quiz_id) {
-
-}
-
-function jquiz_key_in_dict(key, dict) {
-    // The following code is required because checking if key is in
-    // dict, for quizzes, always gives false, even when it should be true
-    id_in_results = false;
-    ids = Object.keys(dict);
-    ids.forEach(k => {
-        if (key == k)
-            id_in_results = true;
-    });
-    return id_in_results
-}
-
-function jquiz_init_quiz(quiz_id) {
-    console.log("JQuiz: Starting quiz");
-    quiz_results[quiz_id] = {
-        'q_index': 0,
-        'completed': false,
-        'score': 0,
-        'num_questions': quizzes[url]['quiz']['questions'].length};
-}
-
-function jquiz_lock_it_in(quiz_id) {
-    // Check the answer, update the score etc.
-    user_correct = true;
-    selector = "#jquiz-" + quiz_id + " input[type=radio]";
-    answers = document.querySelectorAll(selector);
-    answers.forEach(answer => {
-        answer.disabled = true;
-        is_answer = (answer.getAttribute("correct") == "true");
-        if (is_answer)
-            answer.classList.add("correct");
-        else
-            answer.classList.add("incorrect");
-        right_answer = (is_answer == answer.checked);
-        user_correct = user_correct && right_answer;
-    });
-    if (user_correct) {
-        quiz_results[quiz_id]['score']++;
-        audio_applause.pause(); // In case it's still playing
-        audio_applause.currentTime = 0;
-        audio_applause.play();
-    } else {
-
-    }
-    console.log("User is", user_correct);
-    // Enable the "next question" button.
-    selector = "#jquiz-" + quiz_id + " .jquiz-next";
-    next_button = document.querySelector(selector);
-    next_button.disabled = false;
-    selector = "#jquiz-" + quiz_id + " .jquiz-lock";
-    lock_button = document.querySelector(selector);
-    lock_button.disabled = true;
-    selector = "#jquiz-" + quiz_id + " .jquiz-score";
-    score_text = document.querySelector(selector);
-    score_text.innerHTML = "Score: " + quiz_results[quiz_id]['score'];
 }
