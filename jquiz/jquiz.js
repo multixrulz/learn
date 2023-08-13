7a0bdfde5ddb841e40186af75478cddb4931a087
Jared Henley <jared.henley@gmail.com>
2023-08-13 20:41:36 +1000

    Updated site 2023-08-13

--- jquiz/jquiz.js
+++ jquiz/jquiz.js
@@ -389,3 +389,11 @@ function jquiz_parse_file(text) {
     });
     return quiz;
 }
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
